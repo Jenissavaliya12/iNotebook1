7a4fb595f38a058565ba53537f5dc440697c382b
Jenissavaliya12 <savaliyajenis12@gmail.com>
2023-04-11 18:04:45 +0530

    new changes1

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port =  5000;
 
 app.use(cors())
 app.use(express.json());//for using req body
